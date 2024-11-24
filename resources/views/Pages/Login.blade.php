7324b33ac2b2a6d4e57a573748899f50ba180216
Pheaktra0123 <chheangpheaktra168@gmail.com>
2024-11-24 17:08:08 +0700

    design static login and sign up

--- resources/views/Pages/Login.blade.php
+++ resources/views/Pages/Login.blade.php
@@ -8,28 +8,45 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>login</title>
 </head>
-<body class="bg-black ">
-        <main class="bg-white w-11/12 flex-col mx-auto ">
-            <div class=" text-center space-y-4">
-                <h1 class="text-5xl font-bold ">Welcome</h1>
-                <p class="text-xl">Be the first to know about new releases, limited editions and exclusive deals.</p>
-                <h2 class="text-2xl font-bold">Log In</h2>
-            </div>
-            <div>
-                <form action="" >
-                    <div class="">
-                       <div>Email</div>
-                        <div>
-                            <input type="email" required>
+<body class="bg-black">
+        <main class="h-screen flex-col content-center">
+            <div class="bg-white w-1/4 mx-auto rounded-lg  ">
+                <div class="text-center space-y-2 py-3">
+                    <h1 class="text-4xl font-bold">Welcome</h1>
+                    <p class="text-md">Be the first to know about new releases, limited editions and exclusive deals.</p>
+                    <h2 class="text-2xl font-bold mb-2">Log In</h2>
+                </div>
+                <div class="bg-gray-100 px-10 py-10 rounded-b-lg mt-2">
+                    <form class="" >
+                        <div class="w-full">
+                            <div>
+                                <span>*</span>
+                                Email
+                            </div>
+                            <div class="w-full border-b-2 outline-none ">
+                                <input type="email" required class="bg-gray-100 w-full border-none outline-none">
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <div>Password</div>
-                        <div>
-                            <input type="password" required>
+                        <div class="w-full mt-3">
+                            <div>
+                                <span class="text-red">*</span>
+                                Password
+                            </div>
+                            <div class="w-full border-b-2 outline-none">
+                                <input type="password" required class="bg-gray-100 w-full border-none outline-none ">
+                            </div>
                         </div>
-                    </div>
-                </form>
+                        <div class="mt-5 text-sm underline">
+                            <a href="#">Forgot Password</a>
+                        </div>
+                        <div class="w-full bg-black text-white text-center p-2 mt-5 rounded-md">
+                            <button class="w-full">Login</button>
+                        </div>
+                        <div class="text-sm mt-3 text-center">
+                            <p>Don't have account ? <a class="underline" href="/signup">Sign Up</a> </p>
+                        </div>
+                    </form>
+                </div>
             </div>
         </main>
 </body>
