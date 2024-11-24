eb69ed5fe27192006952c6dfd03d997bfc0bb874
Pheaktra0123 <chheangpheaktra168@gmail.com>
2024-11-24 14:33:10 +0700

    create folder

---
+++ resources/views/Pages/Login.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>login</title>
+</head>
+<body>
+        <main>
+            <div class="">
+                <h1>Welcome</h1>
+                <p>Be the first to know about new releases, limited editions and exclusive deals.</p>
+                <h2>Log In</h2>
+            </div>
+            <div>
+                <form action="" >
+
+                </form>
+            </div>
+        </main>
+</body>
+</html>
