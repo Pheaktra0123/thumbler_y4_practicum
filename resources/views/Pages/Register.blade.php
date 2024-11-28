4f14f2258a795938ccab95574f5f3b2284e588a1
Pheaktra0123 <chheangpheaktra168@gmail.com>
2024-11-28 23:36:36 +0700

    validate sign up not yet

--- resources/views/Pages/Register.blade.php
+++ resources/views/Pages/Register.blade.php
@@ -17,14 +17,16 @@
             <h2 class="text-2xl font-bold mb-2">Sign Up</h2>
         </div>
         <div class="bg-gray-100 px-10 py-10 rounded-b-lg mt-2">
-            <form class="" >
+            <form class="" id="signup">
                 <div class="w-full mb-3">
                     <div>
                         <span class="text-red-900">*</span>
                         Username
                     </div>
                     <div class="w-full border-b-2 outline-none ">
-                        <input type="email" required class="bg-gray-100 w-full border-none outline-none">
+                        <input type="text"
+                               id="username"
+                               required class="bg-gray-100 w-full border-none outline-none">
                     </div>
                 </div>
                 <div class="w-full">
@@ -33,7 +35,9 @@
                         Email
                     </div>
                     <div class="w-full border-b-2 outline-none ">
-                        <input type="email" required class="bg-gray-100 w-full border-none outline-none">
+                        <input type="email"
+                               id="email" required
+                               class="bg-gray-100 w-full border-none outline-none">
                     </div>
                 </div>
                 <div class="w-full mt-3">
@@ -42,9 +46,16 @@
                         Password
                     </div>
                     <div class="w-full border-b-2 outline-none">
-                        <input type="password" required class="bg-gray-100 w-full border-none outline-none ">
+                        <input type="password"
+                               id="password"
+                               required
+                               class="bg-gray-100 w-full border-none outline-none"
+                        >
                     </div>
                 </div>
+                <p class="text-[11px] text-red-900 mt-3 hidden " id="warning">
+                    Password must contain number,uppercase,lowercase letter, and at least 8 characters
+                </p>
                 <div class="w-full bg-black text-white text-center p-2 mt-5 rounded-md">
                     <button class="w-full">Sign Up</button>
                 </div>
@@ -56,4 +67,41 @@
     </div>
 </main>
 </body>
+<script>
+    const password = document.getElementById('password');
+    const warning=document.getElementById('warning');
+    const signup=document.getElementById('signup');
+    password.onblur=function (){
+        warning.style.display="none";
+    }
+    signup.addEventListener("submit",function (event){
+        event.preventDefault();
+        let LowerCaseLetters=/[a-z]/g;
+        if(password.value.match(LowerCaseLetters)){
+            warning.style.display="none";
+        }else {
+            warning.style.display="block";
+        }
+        let Numbers=/[0-9]/g
+        if(password.value.match(Numbers)){
+            warning.style.display="none"
+        }
+        let UpperCaseLetters=/[A-Z]/g
+        if(password.value.match(UpperCaseLetters)){
+            warning.style.display="none"
+        }
+        else{
+            warning.style.display="block"
+        }
+        if(password.value.length >= 8) {
+            warning.style.display="none"
+        } else {
+            warning.style.display="block"
+        }
+    });
+
+    password.onkeyup=function (){
+
+    }
+</script>
 </html>
