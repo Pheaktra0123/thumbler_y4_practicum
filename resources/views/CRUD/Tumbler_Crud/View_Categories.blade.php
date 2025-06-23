7816dc8df60148812ee599301050e8755f69ef10
CHHEANG PHEAKTRA <116471248+Pheaktra0123@users.noreply.github.com>
2025-06-23 14:50:31 +0700

    rename

--- resources/views/CRUD/Tumbler_Crud/View_Categories.blade.php
+++ resources/views/CRUD/Tumbler_Crud/View_Categories.blade.php
@@ -19,7 +19,7 @@
 <body>
     <main>
         <section class="w-full mb-10">
-            <h1 class="text-4xl text-gray-800 uppercase text-center font-medium ">Model Tumbler</h1>
+            <h1 class="text-4xl text-gray-800 uppercase text-center font-medium ">Category Tumbler</h1>
         
             <div class="w-11/12 mx-auto flex justify-between item-center place-items-center mt-5">
                 <div class="flex">
@@ -53,7 +53,7 @@
                         <!-- Modal header -->
                         <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-200">
                             <h3 class="text-lg font-semibold text-gray-900">
-                                Create New  Model
+                                Create New  Category
                             </h3>
                             <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-toggle="crud-modal">
                                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -68,7 +68,7 @@
                             @csrf
                             <div class="grid gap-4 mb-4 grid-cols-2">
                                 <div class="col-span-2">
-                                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Model name</label>
+                                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Category name</label>
                                     <input type="text" name="category_name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Type Model name" required>
                                 </div>
                                 <div class="col-span-2">
@@ -98,7 +98,7 @@
                                     <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
                                     </svg>
-                                    <span>Add new model</span>
+                                    <span>Add new category</span>
                                 </div>
 
                                 <!-- Loading State -->
